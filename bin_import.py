6168585f60e83e6ae59ab3bb001c20583681a359
CHAOS-mnt <114514@yjsnpi.monster>
2024-11-01 00:28:58 +0900

    modified for pc version

--- bin_import.py
+++ bin_import.py
@@ -4,15 +4,15 @@ import struct
 import os
 import pathlib
 
-path = pathlib.Path('./Output/')
+path = pathlib.Path('./')
 
 def walk(adr):
 	mylist=[]
 	for root,dirs,files in os.walk(adr):
 		for name in files:
-			if name[-6:] != '.binu8':
+			if name[-4:] != '.bin':
 				continue
-			if name == '__global.binu8':
+			if name == '__global.bin':
 				continue
 			adrlist=os.path.join(root, name)
 			mylist.append(adrlist)
@@ -24,14 +24,13 @@ def byte2int(byte):
 	return long
 
 def dumpstr(src):
-    bstr = b''
-    len = src.read(4)
-    c = src.read(1)
-    while c != b'\x00':
-        bstr += c
-        c = src.read(1)
-    return bstr.decode('utf-8')
-
+	bstr = b''
+	c = src.read(1)
+	while c != b'\x00':
+		bstr += c
+		c = src.read(1)
+	return bstr.decode('utf-8')
+	
 def dumptxt(src, offset, count):
 	src.seek(offset)
 	str_list = []
@@ -40,33 +39,38 @@ def dumptxt(src, offset, count):
 	return str_list
 
 def main():
-	f_lst = walk('Script')
+	f_lst = walk('Script.pac_unpack')
 
 	for fn in f_lst:
-		src = open(fn, 'rb')
-		dstname = fn[:-6] + '.txt'
-		txt = open(dstname, 'r', encoding='utf-8')
+		src = open(fn, 'rb') # origin bin file
+		print('Open',fn)
+		
+		dstname = fn[:-4] + '.txt'
+		txt = open(dstname, 'r', encoding='utf-8') # exported txt file
+		print('Open',dstname)
+		
 		filesize=os.path.getsize(fn)
-		src.seek(4)
+		src.seek(4+17) # header 17bytes + 4bytes
 		entry_count = byte2int(src.read(4))
-		str_offset = (entry_count << 1) * 4 + 8
+		#print(entry_count)
+		str_offset = (entry_count << 1) * 4 + 8 + 17 # where strings begin
 		src.seek(0)
-		data=src.read(str_offset+9)
-		dst = open(path.joinpath(fn[:-6]+'.binu8'),'wb')
+		data=src.read(str_offset + 5)
+		dst = open(path.joinpath(fn[:-4]+'.bin'),'wb') # new bin file
 		dst.write(data)
 		for rows in txt:
 			if rows[0] != '●':
 				continue
 			row = txt.readline().rstrip('\r\n').replace('\\n', '\n').replace('\\r', '\r')
-
 			str = bytes(row, 'utf-8')
-			dst.write(struct.pack('L', len(str)+1))
-			dst.write(struct.pack("%ds" % len(str), str))
-			dst.write(struct.pack('B',0))
+			#dst.write(struct.pack('L', len(str)+1)) # write string len + \x00
+			#dst.write(struct.pack("B", str)) # write string as bytes
+			dst.write(str)
+			dst.write(struct.pack('B',0)) # write \x00
 
 		src.seek(str_offset)
 		str_count = byte2int(src.read(4))
-		dumptxt(src, src.tell()+5, str_count-1)
+		dumptxt(src, src.tell()+1, str_count-1)
 		data=src.read(filesize-src.tell())
 		dst.write(data)
 		src.close()
